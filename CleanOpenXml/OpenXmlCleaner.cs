3bf2e34edd05bee591d096002d851a1377cdf479
Jarosław Kuchta <qhta123@outlook.com>
2024-12-19 09:17:53 +0100

    Prepare to introduce RunInText

---
+++ CleanOpenXml/OpenXmlCleaner.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+using Qhta.OpenXmlTools;
+
+namespace ScanOpenXmlTypes;
+
+public class OpenXmlCleaner : DocumentCleaner
+{
+
+  public void CleanDocxFiles()
+  {
+    string workingDirectory = Directory.GetCurrentDirectory();
+    var docxFiles = Directory.GetFiles(workingDirectory, "*.docx");
+    foreach (var schemaFile in docxFiles)
+    {
+      CleanDocxFile(schemaFile);
+    }
+  }
+
+  public void CleanDocxFile(string docxFileName)
+  {
+    string workingDirectory = Directory.GetCurrentDirectory();
+    docxFileName = Path.Combine(workingDirectory, docxFileName);
+    if (VerboseLevel > 0)
+      Console.WriteLine($"Cleaning documentation file {docxFileName}");
+    var newDocxDirectory = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(docxFileName)!)!,"Done");
+    var newDocxFileName = Path.Combine(newDocxDirectory, Path.GetFileName(docxFileName));
+    if (!Directory.Exists(newDocxDirectory))
+      Directory.CreateDirectory(newDocxDirectory);
+    var tryCount = 3;
+    while (tryCount > 0)
+    {
+      try
+      {
+        File.Copy(docxFileName, newDocxFileName, true);
+        tryCount = 0;
+      } catch (Exception e)
+      {
+        Console.WriteLine(e);
+        tryCount--;
+      }
+    }
+    CleanDocument(newDocxFileName);
+  }
+
+}
---
+++ CleanOpenXml/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+using DocumentFormat.OpenXml.Wordprocessing;
+using Qhta.OpenXmlTools;
+
+namespace ScanOpenXmlTypes;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    var docxPath = Assembly.GetExecutingAssembly().Location;
+    while (docxPath!.Contains("bin"))
+    {
+      docxPath = Path.GetDirectoryName(docxPath);
+    }
+    //docxPath = docxPath.Substring(0, docxPath.LastIndexOf('\\'));
+
+    docxPath = Path.Combine(docxPath, "CleanIt");
+    Directory.SetCurrentDirectory(docxPath);
+    var cleaner = new OpenXmlCleaner();
+    var argList = args.ToList();
+    var verboseLevel = 1;
+    if (argList.Count > 0)
+    {
+      var k = argList.IndexOf("-v");
+      if (k >= 0)
+      {
+        if (k < argList.Count - 1)
+        {
+          verboseLevel = int.Parse(argList[k + 1]);
+          argList.RemoveAt(k + 1);
+        }
+        argList.RemoveAt(k);
+      }
+    }
+    if (argList.Count > 0)
+    {
+      cleaner.VerboseLevel = verboseLevel;
+      cleaner.CleanDocxFile(args[0]);
+    }
+    else
+    {
+      cleaner.VerboseLevel = verboseLevel;
+      cleaner.CleanDocxFiles();
+    }
+  }
+
+}
+
