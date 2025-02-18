125d3f67d407fa27b9459c7ead85e267a23729bd
Jarosław Kuchta <qhta123@outlook.com>
2025-02-18 11:40:49 +0100

    Cyryllic blocks tested

--- TestTextProcessor/TestCharNames.cs
+++ TestTextProcessor/TestCharNames.cs
@@ -79,14 +79,31 @@ public partial class TextProcessingTester
     //count += TestCharLongNamesLike("HALFWIDTH*", ref failed, options);
     //count += TestCharLongNamesLike("FULLWIDTH*", ref failed, options);
 
-    //count += TestCharNamesInRange(0x0000, 0x007F, ref failed, options);
-    //count += TestCharNamesInRange(0x0080, 0x00FF, ref failed, options);
-    //count += TestCharNamesInRange(0x0100, 0x017F, ref failed, options);
-    //count += TestCharNamesInRange(0x0180, 0x024F, ref failed, options);
-    //count += TestCharNamesInRange(0x0250, 0x02AF, ref failed, options);
-    //count += TestCharNamesInRange(0x02B0, 0x02FF, ref failed, options);
-    //count += TestCharNamesInRange(0x0300, 0x036F, ref failed, options);
-    count += TestCharNamesInRange(0x0370, 0x03FF, ref failed, options);
+    //count += TestCharNamesInRange(0x0000, 0x007F, ref failed, options); // Basic Latin
+    //count += TestCharNamesInRange(0x0080, 0x00FF, ref failed, options); // Latin-1 Supplement
+    //count += TestCharNamesInRange(0x0100, 0x017F, ref failed, options); // Latin Extended-A
+    //count += TestCharNamesInRange(0x0180, 0x024F, ref failed, options); // Latin Extended-B
+    //count += TestCharNamesInRange(0x0250, 0x02AF, ref failed, options); // IPA Extensions
+    //count += TestCharNamesInRange(0x02B0, 0x02FF, ref failed, options); // Spacing Modifier Letters
+    //count += TestCharNamesInRange(0x0300, 0x036F, ref failed, options); // Combining Diacritical Marks
+    //count += TestCharNamesInRange(0x0370, 0x03FF, ref failed, options); // Greek and Coptic
+    //count += TestCharNamesInRange(0x0400, 0x04FF, ref failed, options); // Cyrillic
+    count += TestCharNamesInRange(0x0500, 0x052F, ref failed, options); // Cyrillic Supplement
+    //count += TestCharNamesInRange(0x0530, 0x058F, ref failed, options); // Armenian
+    //count += TestCharNamesInRange(0x0590, 0x05FF, ref failed, options); // Hebrew
+    //count += TestCharNamesInRange(0x0600, 0x06FF, ref failed, options); // Arabic
+    //count += TestCharNamesInRange(0x0700, 0x074F, ref failed, options); // Syriac
+    //count += TestCharNamesInRange(0x0750, 0x077F, ref failed, options); // Arabic Supplement
+    //count += TestCharNamesInRange(0x0780, 0x07BF, ref failed, options); // Thaana
+    //count += TestCharNamesInRange(0x07C0, 0x07FF, ref failed, options); // NKo
+    //count += TestCharNamesInRange(0x0800, 0x083F, ref failed, options); // Samaritan
+    //count += TestCharNamesInRange(0x0840, 0x085F, ref failed, options); // Mandaic
+    //count += TestCharNamesInRange(0x0860, 0x086F, ref failed, options); // Syriac Supplement
+    //count += TestCharNamesInRange(0x0870, 0x089F, ref failed, options); // Thaana
+    //count += TestCharNamesInRange(0x08A0, 0x08FF, ref failed, options); // Arabic Extended-A
+    //count += TestCharNamesInRange(0x0900, 0x097F, ref failed, options); // Devanagari
+
+
     //count += TestCharNamesInRange(0xFF00, 0xFFEF, ref failed, options);
 
     //count += TestCharNamesInRange(0x1FA70, 0x1FAFF, ref failed, options);
