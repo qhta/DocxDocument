1ea039033f38b757934b1d24298f13c5d07eaba6
Jarosław Kuchta <qhta123@outlook.com>
2026-01-07 23:38:19 +0100

    ElementCollections types declared

--- DocumentModel.Interfaces/Packaging/Classes/OpenXmlPart.cs
+++ DocumentModel.Interfaces/Packaging/Classes/OpenXmlPart.cs
@@ -3,7 +3,6 @@ namespace DocumentModel.Packaging;
 /// <summary>
 ///   Represents an abstract base interface for all OpenXml parts.
 /// </summary>
-public interface OpenXmlPart
 {
   /// <summary>
   ///   Gets the internal part path in the package.
