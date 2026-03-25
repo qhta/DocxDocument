8ff0fc1dff277ec6489d6d13019b9975ccf5bd3b
Jarosław Kuchta <qhta123@outlook.com>
2026-03-25 21:21:51 +0100

    WordInterfaces spit to subfolders

--- DocumentModel.Interfaces/WordInterfaces/Editors/Editors.cs
+++ DocumentModel.Interfaces/WordInterfaces/Editors/Editors.cs
@@ -4,11 +4,6 @@ namespace DocumentModel.Interop.Word;
 /// A collection of Editor objects that represents a collection of users or groups of users who have been given specific permissions to edit portions of a document.
 /// </summary>
 /// <seealso cref="https://learn.microsoft.com/en-us/dotnet/api/microsoft.office.interop.word.editors?view=word-pia"/>
-public partial interface Editors : InteropObject
+public partial interface Editors : InteropCollection<Editor>
 {
-  /// <summary>
-  /// The count.
-  /// </summary>
-  /// <seealso cref="https://learn.microsoft.com/en-us/dotnet/api/microsoft.office.interop.word.editors.count?view=word-pia"/>
-  public int Count { get; }
 }
